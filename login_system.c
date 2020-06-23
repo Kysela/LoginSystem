39d9cd3494a893ada197010d1e5b00f2b4bc5d71
František Kysela <bythedroid@gmail.com>
2020-06-23 03:47:52 +0200

    push new trash

--- login_system.c
+++ login_system.c
@@ -5,13 +5,44 @@
 #include <sys/stat.h>
 
 struct option_table {
-		const char *varName;
+		const char *optionName;
 	} table[] = {
 		{ "1) Login" },
 		{ "2) Register" },
 		{ "3) Exit program" },
 		{ ""},
 	};
+	
+int to_int(const char* str) {
+	int ret = 0, sign = 1;
+	if( *str == '+' || *str == '-' ) {
+       if( *str == '-' ) sign = -1;
+       str++;
+    }
+    while(*str) {
+        ret = (ret << 4) - (ret << 2) - (ret << 1) + (*str++ - '0');
+    }
+    return ret * sign;
+}
+
+bool is_number(const char* s) {
+const char* p = s;
+if( *p == '+' || *p == '-' ) p++;
+while (*p != '\0') {
+if (*p != '.' && !((uint8_t)(*p) - '0' <= 9))
+return false;
+p++;
+}
+return true;
+}
+	
+int argument_supported(const char* arg) {
+int table_position = 0;
+const struct option_table *options = table;
+while (options->optionName) {
+    table_position++;
+	if (to_int(arg) )
+}
 
 int main(int argc, char** argv) {
    int arg_error = 0;
@@ -27,9 +58,7 @@ int main(int argc, char** argv) {
 	printf("2) Register\n");
 	printf("3) Exit program\n\n");
 	
-	const struct option_table *options = table;
-	while (options->type) {
-		if (environment->type == kEnv) {
+
 	
 	
 char choice[512];
@@ -39,6 +68,7 @@ do {
            continue;
  } while (strcmp(choice, "1") != 0 && strcmp(choice, "2") != 0 && strcmp(choice, "3") != 0);
  
+
 
 return 0;
 }
